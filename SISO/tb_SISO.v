4993c31b7716b9cdc01c0764157adff8d65d7110
Madhu Visagan H T <visaganhtm300507@gmail.com>
2026-06-05 23:02:48 +0530

    Update tb_SISO.v

--- SISO/tb_SISO.v
+++ SISO/tb_SISO.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.06.2026 18:21:44
-// Design Name: 
-// Module Name: tb_SISO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_SISO(
 
