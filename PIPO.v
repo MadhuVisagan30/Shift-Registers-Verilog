22a09c99b62f00c6b47a659eb8d995d5680517ed
Madhu Visagan H T <visaganhtm300507@gmail.com>
2026-06-05 22:57:09 +0530

    Add files via upload

---
+++ PIPO.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.06.2026 00:19:48
+// Design Name: 
+// Module Name: PIPO
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PIPO(input clk,reset,input [3:0] D,output [3:0] Q);
+    d_ff one(clk,D[0],reset,Q[0]);
+    d_ff two(clk,D[1],reset,Q[1]);
+    d_ff three(clk,D[2],reset,Q[2]);
+    d_ff four(clk,D[3],reset,Q[3]);    
+endmodule
+module d_ff(input clk,d,reset,output reg q);
+    always @(posedge clk or posedge reset)begin
+        if(reset)
+            q<=1'b0;
+        else
+            q<=d;
+        end
+        endmodule
